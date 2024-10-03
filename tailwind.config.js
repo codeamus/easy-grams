18be8184d980862287d48fdaba306fec87c84886
Alexander Urrutia <matias04041994@gmail.com>
2024-10-03 18:01:37 -0300

    Add font families and font sizes to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,15 @@ module.exports = {
           text: '#ffffff',
         },
       },
+      fontFamily: {
+        poppinsMedium: ['Poppins_500Medium'],
+        poppinsRegular: ['Poppins_400Regular'],
+        poppinsSemiBold: ['Poppins_600SemiBold'],
+      },
+      fontSize: {
+        '4xl': ['2.35rem', { lineHeight: '3.75rem' }],
+        '5xl': ['3.25rem', { lineHeight: '3.75rem' }],
+      },
     },
   },
   plugins: [],
