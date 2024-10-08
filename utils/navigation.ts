dded3b4c0fc242bb6faeaa11b2ac90a1f833317a
Alexander Urrutia <matias04041994@gmail.com>
2024-10-08 18:33:08 -0300

    Refactor navigation utility function and add optionsStack function

--- utils/navigation.ts
+++ utils/navigation.ts
@@ -1,3 +1,5 @@
+import { RouteProp, ParamListBase } from '@react-navigation/native';
+import { NativeStackNavigationOptions } from '@react-navigation/native-stack';
 import { useRouter, Href } from 'expo-router';
 
 export const useAppNavigation = () => {
@@ -20,4 +22,30 @@ export const useAppNavigation = () => {
     replaceTo,
     goBack,
   };
+};
+
+type OptionsStack = {
+  title: string;
+};
+
+export const optionsStack = ({ title }: OptionsStack) => {
+  return (props: {
+    route: RouteProp<ParamListBase, string>;
+    navigation: any;
+  }): NativeStackNavigationOptions => ({
+    headerShown: true, // Mostrar el header solo en login
+    title, // Título del header
+    headerBackVisible: true, // Mostrar la flecha de "volver"
+    headerBackTitleVisible: false, // Ocultar el texto "Volver"
+    headerTitleStyle: {
+      fontFamily: 'Poppins_600SemiBold', // Fuente personalizada
+      fontSize: 18,
+      color: '#0d1220', // Color del texto
+    },
+    headerTintColor: '#000', // Color de la flecha de "volver"
+    headerStyle: {
+      backgroundColor: '#fff', // Asegurar que el color de fondo sea consistente
+    },
+    headerShadowVisible: false, // Eliminar la sombra en todas las plataformas
+  });
 };
