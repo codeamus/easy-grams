73010d449329e7ba00360a8dc9b56578aabb1ac5
Alexander Urrutia <matias04041994@gmail.com>
2024-10-08 18:33:16 -0300

    Refactor SigninLayout component to use optionsStack function for login screen

--- app/signin/_layout.tsx
+++ app/signin/_layout.tsx
@@ -1,5 +1,7 @@
 import { Stack } from 'expo-router';
 
+import { optionsStack } from '~/utils/navigation';
+
 export default function SigninLayout() {
   return (
     <Stack
@@ -7,20 +9,7 @@ export default function SigninLayout() {
         headerShown: false, // Ocultar el header por defecto en todas las pantallas
       }}>
       {/* Aquí puedes configurar específicamente el header de la pantalla login */}
-      <Stack.Screen
-        name="login"
-        options={{
-          headerShown: true, // Mostrar el header solo en login
-          title: 'Iniciar sesión', // Título del header
-          headerBackVisible: true, // Mostrar la flecha de "volver"
-          headerBackTitleVisible: false, // Ocultar el texto "Volver"
-          headerTitleStyle: {
-            fontFamily: 'Poppins_600SemiBold', // Fuente personalizada
-            fontSize: 20,
-            color: '#0d1220', // Color del texto
-          },
-        }}
-      />
+      <Stack.Screen name="login" options={optionsStack({ title: 'Login' })} />
     </Stack>
   );
 }
--- app/signin/login.tsx
+++ app/signin/login.tsx
@@ -2,7 +2,7 @@ import { View, Text } from 'react-native';
 
 const Login = () => {
   return (
-    <View>
+    <View className="flex-1 bg-white">
       <Text>Login</Text>
     </View>
   );
