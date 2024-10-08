f803a6c24c1c80e217c2b4e6aeb3a76c5aff8b51
Alexander Urrutia <matias04041994@gmail.com>
2024-10-08 17:59:53 -0300

    Refactor SigninLayout component to customize header options for the login screen

--- app/signin/_layout.tsx
+++ app/signin/_layout.tsx
@@ -1,7 +1,26 @@
-import '../../global.css';
-
 import { Stack } from 'expo-router';
 
 export default function SigninLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false, // Ocultar el header por defecto en todas las pantallas
+      }}>
+      {/* Aquí puedes configurar específicamente el header de la pantalla login */}
+      <Stack.Screen
+        name="login"
+        options={{
+          headerShown: true, // Mostrar el header solo en login
+          title: 'Iniciar sesión', // Título del header
+          headerBackVisible: true, // Mostrar la flecha de "volver"
+          headerBackTitleVisible: false, // Ocultar el texto "Volver"
+          headerTitleStyle: {
+            fontFamily: 'Poppins_600SemiBold', // Fuente personalizada
+            fontSize: 20,
+            color: '#0d1220', // Color del texto
+          },
+        }}
+      />
+    </Stack>
+  );
 }
