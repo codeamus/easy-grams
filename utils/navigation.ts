9597b5015f4c452e041f2ac3030b6c2fd4c41847
Alexander Urrutia <matias04041994@gmail.com>
2024-10-08 17:28:22 -0300

    Add utility function for app navigation

---
+++ utils/navigation.ts
@@ -0,0 +1,24 @@
+// utils/navigation.ts
+import { useRouter, Href } from 'expo-router';
+
+export const useAppNavigation = () => {
+  const router = useRouter();
+
+  const navigateTo = (path: Href) => {
+    router.push(path);
+  };
+
+  const replaceTo = (path: Href) => {
+    router.replace(path);
+  };
+
+  const goBack = () => {
+    router.back();
+  };
+
+  return {
+    navigateTo,
+    replaceTo,
+    goBack,
+  };
+};
