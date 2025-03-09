ad649040e8d8bd9fa06a7628ef4966951ccc2e78
Alexander Urrutia <matias04041994@gmail.com>
2025-03-09 18:16:22 -0300

    Refactor ModalSigIn component to improve structure and enhance user experience

--- components/common/ModalSigIn.tsx
+++ components/common/ModalSigIn.tsx
@@ -1,35 +1,38 @@
-import { View, Text, Modal, Button, TouchableOpacity } from 'react-native';
-import IconGmail from './icons/IconGmail';
+import { View, Text, Modal, TouchableOpacity, TouchableWithoutFeedback } from 'react-native';
+
 import IconEmail from './icons/IconEmail';
+import IconGmail from './icons/IconGmail';
 
 const ModalSigIn = ({ modalVisible, setModalVisible }) => {
   return (
     <Modal
       animationType="fade"
-      transparent={true}
+      transparent
       visible={modalVisible}
       onRequestClose={() => setModalVisible(false)}>
-      <View className="bg-dark-modalBackground flex-1 items-center justify-end">
-        <View className="flex w-full items-center gap-4 rounded-xl bg-white px-6 py-12">
-          <TouchableOpacity
-            className="absolute right-6 top-2"
-            onPress={() => setModalVisible(false)}>
-            <Text className="text-dark-onSurfacePrimary text-xl">X</Text>
-          </TouchableOpacity>
-          <View className="border-dark-outline w-full flex-row items-center justify-center gap-2 rounded-xl border py-4">
-            <IconGmail />
-            <Text className="font-poppinsRegular text-dark-onSurfacePrimary">
-              Iniciar con Google
-            </Text>
-          </View>
-          <View className="border-dark-outline w-full flex-row items-center justify-center gap-2 rounded-xl border py-4">
-            <IconEmail />
-            <Text className="font-poppinsRegular text-dark-onSurfacePrimary">
-              Iniciar con Correo
-            </Text>
+      <TouchableWithoutFeedback onPress={() => setModalVisible(false)}>
+        <View className="flex-1 items-center justify-end bg-dark-modalBackground">
+          <View className="flex w-full items-center gap-4 rounded-xl bg-white px-6 py-12">
+            <TouchableOpacity
+              className="absolute right-6 top-2"
+              onPress={() => setModalVisible(false)}>
+              <Text className="text-xl text-dark-onSurfacePrimary">X</Text>
+            </TouchableOpacity>
+            <View className="w-full flex-row items-center justify-center gap-2 rounded-xl border border-dark-outline py-4">
+              <IconGmail />
+              <Text className="font-poppinsRegular text-dark-onSurfacePrimary">
+                Iniciar con Google
+              </Text>
+            </View>
+            <View className="w-full flex-row items-center justify-center gap-2 rounded-xl border border-dark-outline py-4">
+              <IconEmail />
+              <Text className="font-poppinsRegular text-dark-onSurfacePrimary">
+                Iniciar con Correo
+              </Text>
+            </View>
           </View>
         </View>
-      </View>
+      </TouchableWithoutFeedback>
     </Modal>
   );
 };
